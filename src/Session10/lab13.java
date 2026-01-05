c22e0f335fcb88fbd72daafb5ecd3b1459ea81a4
Haseen Khan <haseen.khan@technoduxx.com>
2026-01-05 18:00:54 +0530

    2nd commit

---
+++ src/Session10/lab13.java
@@ -0,0 +1,14 @@
+package Session10;
+
+public class lab13 {
+
+    public static void main(String[] args) {
+
+
+        int num = 33;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num+" "+"x"+" "+i+" "+"="+" "+ num * i);
+        }
+    }
+}
